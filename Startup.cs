913402f7a180b7d3de8f9436156ddd30e6af94c2
LeoKnox <LeoKnox@users.noreply.github.com>
2021-07-09 20:30:09 -0700

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace AdvApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RoomContext>(opt => opt.UseInMemoryDatabase("RoomList"));
             services.AddControllers();
         }
 
