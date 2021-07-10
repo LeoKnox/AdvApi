91c80faaae3a004a3748aaeb317a406786d13e3b
LeoKnox <LeoKnox@users.noreply.github.com>
2021-07-09 20:24:36 -0700

    Add files via upload

---
+++ Models/Room.cs
@@ -0,0 +1,10 @@
+namespace AdvApi.Models
+{
+    public class RoomItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Visited {get; set; }
+    }
+}
